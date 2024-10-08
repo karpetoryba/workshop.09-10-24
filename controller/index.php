b7224913897d750e8729c15d9c2508babc87bfcf
David Robert <contact@david-robert.fr>
2024-10-08 15:51:48 +0200

    mvc create order

--- controller/index.php
+++ controller/index.php
@@ -1,14 +1,4 @@
 <?php
 
-require_once '../model/Order.php';
-
-try {
-	$order = new Order('Jean Pierre', ['iphone', 'chaise', 'table', 'bureau', 'lampe']);
-
-	require_once '../view/order-created.php';
-
-} catch (Exception $e) {
-
-	require_once '../view/order-error.php';
-}
+require_once '../view/home.php';
 
